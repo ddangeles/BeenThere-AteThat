4728205c22c3eb13dbb2341e5f51ae3f32ae2e07
Vincent Fernandez <fernandez97vincent@gmail.com>
2022-10-12 11:10:19 -0700

    added comment route

---
+++ routes/api/commentRoutes.js
@@ -0,0 +1,69 @@
+const { Comment } = require("../../models/");
+const router = require('express').Router();
+const withAuth = require("../../utils/withAuth");
+
+// get all posted comments
+
+router.get("/", withAuth, async (req, res) => {
+try {
+  const commentData = await Comment.findAll();
+  const comments = commentData.map((comment) => comment.get({ plain: true }));
+    //res.render("all-comments", { comments, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// create comments 
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      userId: req.session.userId,
+    });
+    res.json(newComment);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// edit comments
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      const [affectedRows] = await Comment.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (affectedRows === 1) {
+        res.sendStatus(200);
+      } else {
+        res.sendStatus(404);
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // delete comments
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: "No comment found with this id!" });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
