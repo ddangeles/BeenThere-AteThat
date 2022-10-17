70db0f127c2b40360e23c208888ce517ece41bd7
donnadangeles@gmail.com <donnadangeles@gmail.com>
2022-10-17 10:57:16 -0700

    model changes

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const Restaurant = require('./Restaurant');
 const User = require('./User');
 const Comment = require('./Comment');
 const BeenThere = require('./beenthere');
-const Image = require('./Image')
+
 
 User.hasMany(Restaurant, {
     foreignKey: "userId",
@@ -10,11 +10,6 @@ User.hasMany(Restaurant, {
 });
 
 User.hasMany(Comment, {
-    foreignKey: "userId",
-    onDelete: "CASCADE"
-});
-
-User.hasMany(Image, {
     foreignKey: "userId",
     onDelete: "CASCADE"
 });
@@ -32,11 +27,6 @@ Restaurant.hasMany(Comment, {
     foreignKey: "restaurantId"
 });
 
-Restaurant.hasMany(Image, {
-    foreignKey: "restaurantId"
-});
-
-
 Comment.belongsTo(Restaurant, {
     foreignKey: "restaurantId"
 });
@@ -45,17 +35,9 @@ Comment.belongsTo(User, {
     foreignKey: "userId"
 });
 
-Image.belongsTo(User, {
-    foreignKey: "userId"
-});
-
-Image.belongsTo(Restaurant, {
-    foreignKey: "restaurantId"
-});
-
 BeenThere.belongsTo(User,{
     foreignKey: "userId"
 });
 
 
-module.exports = { Restaurant, User, Comment, BeenThere, Image };
+module.exports = { Restaurant, User, Comment, BeenThere};
